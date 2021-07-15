84f9306fd2a2bacaceaa1ea26af3737d85b4606a
Sierri21 <sierri21@yandex.ru>
2021-07-15 22:17:31 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <search-component />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import searchComponent from './components/search-component.vue'
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    searchComponent
   }
 })
 </script>
---
+++ src/components/search-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'search-component',
+  props: {
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
